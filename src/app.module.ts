c3bef0997ad6c3c95c881be6a96013ef2743a4f4
linyunyi <471177894@qq.com>
2020-07-15 14:43:32 +0800

    Add 静态资源

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,19 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { CompanyModule } from './company/company.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [CatsModule, CompanyModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client'),
+      exclude: ['/api*']
+    }),
+    CatsModule,
+    CompanyModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
