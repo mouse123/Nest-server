209cfe3812254d78bff9bbe1b7d2972939e3d464
linyunyi <471177894@qq.com>
2020-07-15 16:17:57 +0800

    添加开启 静态资源服务器

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
 import { CompanyModule } from './company/company.module';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join } from 'path';
 
 @Module({
   imports: [
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'client'),
-      exclude: ['/api*']
-    }),
     CatsModule,
     CompanyModule
   ],
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as compression from 'compression';
+import { AppStaticModule } from './app-static.module';
 
 async function bootstrap() {
+
+  //启动服务器
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  app.use(compression());
   await app.listen(8089);
+
+  //启动静态服务器
+  const appStatic = await NestFactory.create(AppStaticModule);
+  appStatic.enableCors();
+  appStatic.use(compression());
+  await appStatic.listen(8088);
 }
 bootstrap();
