b696d0507ba462aaa5ed350ac9ac968d36eb9272
linyunyi <471177894@qq.com>
2020-07-17 11:43:51 +0800

    添加数据的curd

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,13 +1,34 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Query, Post, Body, Put, Param, Delete } from '@nestjs/common';
 import { UserService } from './user.service'
 import { User } from './user.entity'
+import { UserDto } from './paramDto/dto';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService) {}
+    constructor(private readonly userService: UserService) { }
 
     @Get('list')
     findAll(): Promise<User[]> {
         return this.userService.findAll();
+    }
+
+    @Get(':id')
+    findOne(@Param('id') id: number): Promise<User[]> {
+        return this.userService.findOne(id);
+    }
+
+    @Post()
+    create(@Body() userDto: UserDto) {
+        return this.userService.create(userDto);
+    }
+
+    @Put(':id')
+    update(@Param('id') id: number, @Body() userDto: UserDto) {
+        return this.userService.update(id, userDto);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+        return this.userService.delete(id);
     }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -2,16 +2,37 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
+import { UserInterface } from './interfaces/user.interface';
 
 @Injectable()
 export class UserService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: Repository<User>,
-  ) {}
+  ) { }
 
   async findAll(): Promise<User[]> {
-    console.log("this.userRepository.find()",await this.userRepository.find())
     return await this.userRepository.find();
+  }
+
+  async findOne(id: number): Promise<User[]> {
+    return await this.userRepository.find({ id: id });
+  }
+
+  async create(userInterface: UserInterface) {
+    const user = new User();
+    user.name = userInterface.name;
+    return await this.userRepository.save(user);
+  }
+
+  async update(id: number, userInterface: UserInterface) {
+    let user = await this.userRepository.findOne(id);
+    user.name = userInterface.name;
+    return await this.userRepository.save(user);
+  }
+
+  async delete(id: number) {
+    let user = await this.userRepository.findOne(id);
+    return await this.userRepository.remove(user);
   }
 }
